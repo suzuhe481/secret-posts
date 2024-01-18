ebb7adb37a19103338acaa1099a9c2e5acad1314
Hector <baseballhector@gmail.com>
2024-01-18 16:41:35 -0500

    Initial route for user

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const user_controller = require("../controllers/userController");
+
+// GET request for a single user.
+router.get("/:id", user_controller.user_detail);
 
 module.exports = router;
