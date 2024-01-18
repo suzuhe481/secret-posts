8cf081f6b08e02d0b07f9df909a3c320ce3b91ea
Hector <baseballhector@gmail.com>
2024-01-17 19:06:17 -0500

    Initial User model

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: {
+    type: String,
+    required: true,
+    minLength: 1,
+  },
+  last_name: {
+    type: String,
+    required: true,
+    minLength: 1,
+  },
+  email: {
+    type: String,
+    required: true,
+    minLength: 1,
+  },
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  status: {
+    type: String,
+    required: true,
+    enum: ["Member", "Member+"],
+  },
+});
+
+// Export module
+module.exports = mongoose.model("User", UserSchema);
