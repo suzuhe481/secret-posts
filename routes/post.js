dbd9d7ba8c657f8eca29e757fb5d957464dfd9c5
Hector <baseballhector@gmail.com>
2024-01-18 16:41:44 -0500

    Initial route for post

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+const post_controller = require("../controllers/postController");
+
+// Get a single post
+router.get("/:id", post_controller.post_detail);
 
 module.exports = router;
