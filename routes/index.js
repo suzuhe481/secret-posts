63f9b9100fe56dc4f665ea2db1ae9f785feadefa
Hector <baseballhector@gmail.com>
2024-01-18 16:41:19 -0500

    Starting routeos for home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const index_controller = require("../controllers/indexController");
+
+// 3 ways to get to home page.
+// GET Home page
+router.get("/", index_controller.index);
+
+// The following 2 GET methods redirect to the above route.
+router.get("/home", function (req, res, next) {
+  res.redirect("/");
+});
+
+router.get("/index", function (req, res, next) {
+  res.redirect("/");
 });
 
 module.exports = router;
