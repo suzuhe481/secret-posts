74809452abc46346f880a4d89ee2cc0990bfd21a
Hector <baseballhector@gmail.com>
2024-01-19 16:38:47 -0500

    Mongoose connection to MongoDB

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ const bcrypt = require("bcryptjs");
 
 const User = require("./models/user");
 
+// Set up a Mongoose connection
+mongoose.set("strictQuery", false);
+
+const dev_db_url = "placeholder";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+// Wait for database connect. Logs error if fail to connect.
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 var indexRouter = require("./routes/index");
 var postRouter = require("./routes/post");
 var userRouter = require("./routes/user");
