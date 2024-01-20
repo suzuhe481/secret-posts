79e963b34a7bf4b1ff8767fc4caae1ba339aa5e6
Hector <baseballhector@gmail.com>
2024-01-19 20:08:35 -0500

    Requireing bcrypt, body, and validationResult

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ const Post = require("../models/post");
 const User = require("../models/user");
 
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 // Gets the details for a single user.
 exports.user_detail = asyncHandler(async (req, res, next) => {
