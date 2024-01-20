a09aad0bb78e521c19dc5d818678f381eb912871
Hector <baseballhector@gmail.com>
2024-01-19 20:03:49 -0500

    GET/POST route for creating a user

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 const user_controller = require("../controllers/userController");
 
+// GET/POST request to create a user.
+// NOTE: This must come before the routes that (uses :id)
+router.get("/create", user_controller.user_create_get);
+router.post("/create", user_controller.user_create_post);
+
 // GET request for a single user.
 router.get("/:id", user_controller.user_detail);
 
