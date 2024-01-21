3b5f06d9cc2d77dea0ffbe857eb73a55a1ebdb9b
Hector <baseballhector@gmail.com>
2024-01-21 16:16:12 -0500

    Added route handler callbacks for admin

--- routes/user.js
+++ routes/user.js
@@ -8,6 +8,10 @@ const user_controller = require("../controllers/userController");
 router.get("/create", user_controller.user_create_get);
 router.post("/create", user_controller.user_create_post);
 
+// GET/POST route-handler callbacks to create an admin user
+router.get("/admin-create", user_controller.admin_user_create_get);
+router.post("/admin-create", user_controller.admin_user_create_post);
+
 // GET request for a single user.
 router.get("/:id", user_controller.user_detail);
 
