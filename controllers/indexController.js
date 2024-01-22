0b3757ec1e3248b8511c988b9f1b818d68e23402
Hector <baseballhector@gmail.com>
2024-01-21 21:06:40 -0500

    Handling the creation of a new post

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -8,3 +8,47 @@ const { body, validationResult } = require("express-validator");
 exports.index = asyncHandler(async (req, res, next) => {
   res.render("index", { title: "Secret Posts" });
 });
+
+// Handles the creation of a new post.
+exports.index_post = [
+  // Validate form data
+  body("post", "Post must not be empty").trim().escape().isLength({ min: 1 }),
+
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors.
+    const errors = validationResult(req);
+
+    // Get the current user.
+    const currUser = User.findOne({ id: req.user._id });
+
+    // Get date and time at time of posting.
+    const date = new Date().toISOString();
+
+    // Create post object with given form data.
+    const post = new Post({
+      post: req.body.post,
+      post_date: date,
+      user: req.user,
+    });
+
+    // There are errors.
+    // Render form again with sanitized values and error messages.
+    if (!errors.isEmpty()) {
+      res.render("/", {
+        title: "Secret Posts",
+        errors: errors.array(),
+      });
+      return;
+    }
+    // Save post and add post to current user's posts.
+    else {
+      await post.save();
+
+      const filter = { _id: req.user._id };
+      const update = { $push: { posts: post } };
+      await User.findOneAndUpdate(filter, update, { new: true });
+
+      res.redirect("/");
+    }
+  }),
+];
