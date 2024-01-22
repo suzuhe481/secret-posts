d4bc7ec885483c9958265f5010789a46eb7cb2ad
Hector <baseballhector@gmail.com>
2024-01-21 21:04:27 -0500

    Requiring body and validationResult from express async handler

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,6 +2,7 @@ const Post = require("../models/post");
 const User = require("../models/user");
 
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // Displays the Secret Posts home page.
 exports.index = asyncHandler(async (req, res, next) => {
