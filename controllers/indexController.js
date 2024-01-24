98d210ad76a3e2695e381db282f09e14d1e09cab
Hector <baseballhector@gmail.com>
2024-01-24 18:14:23 -0500

    Removed body, validationResult

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,7 +2,6 @@ const Post = require("../models/post");
 const User = require("../models/user");
 
 const asyncHandler = require("express-async-handler");
-const { body, validationResult } = require("express-validator");
 
 // Displays the Secret Posts home page.
 exports.index = asyncHandler(async (req, res, next) => {
