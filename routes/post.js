5102bad3376c67988cd6261fbf1987551c2cfdaf
Hector <baseballhector@gmail.com>
2024-01-24 18:15:12 -0500

    Adding post create route

--- routes/post.js
+++ routes/post.js
@@ -6,4 +6,7 @@ const post_controller = require("../controllers/postController");
 // Get a single post
 router.get("/:id", post_controller.post_detail);
 
+// Creating a post POST
+router.post("/create", post_controller.post_create_post);
+
 module.exports = router;
