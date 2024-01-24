6e3ab177697d811e66a667368a249111dc65b6f8
Hector <baseballhector@gmail.com>
2024-01-24 18:12:19 -0500

    Requiring body and validationResult from express validator

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,6 +2,7 @@ const Post = require("../models/post");
 const User = require("../models/user");
 
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // Gets the details for a single post.
 exports.post_detail = asyncHandler(async (req, res, next) => {
