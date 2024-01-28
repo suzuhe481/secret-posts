e213eec98a68d19fc003df533644bea5331da588
Hector <baseballhector@gmail.com>
2024-01-28 17:45:08 -0500

    Added GET/POST routes for post delete

--- routes/post.js
+++ routes/post.js
@@ -9,4 +9,8 @@ router.get("/:id", post_controller.post_detail);
 // Creating a post POST
 router.post("/create", post_controller.post_create_post);
 
+// GET/POST requests to delete a post.
+router.get("/post/:id/delete", post_controller.post_delete_get);
+router.post("/post/:id/delete", post_controller.post_delete_post);
+
 module.exports = router;
