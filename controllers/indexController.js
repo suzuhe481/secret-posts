b49deb994089342c145ba37734bafa3c1c7c1007
Hector <baseballhector@gmail.com>
2024-01-28 18:22:08 -0500

    Populates the MongoDB query with user

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -6,7 +6,7 @@ const asyncHandler = require("express-async-handler");
 // Displays the Secret Posts home page.
 exports.index = asyncHandler(async (req, res, next) => {
   // Retrieves all the posts with the most recent first.
-  const allPosts = await Post.find({}).sort({ post_date: -1 }).exec();
+  const allPosts = await Post.find({}).populate("user").sort({ post_date: -1 }).exec();
 
   res.render("index", {
     title: "Secret Posts",
