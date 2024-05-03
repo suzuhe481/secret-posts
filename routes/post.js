783bc53d8100cf8b24a4a2a25b3354112d86c1e2
Hector <baseballhector@gmail.com>
2024-05-02 20:00:28 -0400

    Moved the create route to be first

--- routes/post.js
+++ routes/post.js
@@ -3,14 +3,14 @@ var router = express.Router();
 
 const post_controller = require("../controllers/postController");
 
-// Get a single post
-router.get("/:id", post_controller.post_detail);
-
 // Creating a post POST
 router.post("/create", post_controller.post_create_post);
 
+// Get a single post
+router.get("/:id", post_controller.post_detail);
+
 // GET/POST requests to delete a post.
-router.get("/post/:id/delete", post_controller.post_delete_get);
-router.post("/post/:id/delete", post_controller.post_delete_post);
+router.get("/:id/delete", post_controller.post_delete_get);
+router.post("/:id/delete", post_controller.post_delete_post);
 
 module.exports = router;
